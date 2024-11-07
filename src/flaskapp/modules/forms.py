1baabc456ee4eea0bdee0ae94fafde8ac77406ad
zahaoid <kurushimifunk@gmail.com>
2024-11-07 04:24:35 +0300

    Quality css/html changes, jinja code refactored, added context fields to the entry submitting form

--- src/flaskapp/modules/forms.py
+++ src/flaskapp/modules/forms.py
@@ -1,19 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField, HiddenField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField, HiddenField, FormField, Form, FieldList, Field
+from wtforms.widgets import TextInput, SubmitInput
 from wtforms.validators import DataRequired, EqualTo, Email, ValidationError, Length
 from flaskapp.main import ar
 from database.main import qr
+
 class LoginForm(FlaskForm):
-    username = StringField(ar.username, validators=[DataRequired()])
-    password = PasswordField(ar.password, validators=[DataRequired()])
+    username = StringField(ar.username + ":", validators=[DataRequired(), Length(max=32)])
+    password = PasswordField(ar.password + ":", validators=[DataRequired(), Length(max=32)])
     remember_me = BooleanField(ar.remember_me)
     submit = SubmitField(ar.login)
 
 class RegisterForm(FlaskForm):
-    username = StringField(ar.username, validators=[DataRequired()])
-    email = StringField(ar.email, validators=[DataRequired(), Email()])
-    password = PasswordField(ar.password, validators=[DataRequired()])
-    password2 = PasswordField("أعد " + ar.password, validators=[DataRequired(), EqualTo("password")])
+    username = StringField(ar.username + ":", validators=[DataRequired(), Length(max=32)])
+    email = StringField(ar.email + ":", validators=[DataRequired(), Email(), Length(max=64)])
+    password = PasswordField(ar.password + ":", validators=[DataRequired(), Length(max=32)])
+    password2 = PasswordField("أعد " + ar.password + ":", validators=[DataRequired(), EqualTo("password"), Length(max=32)])
     remember_me = BooleanField(ar.remember_me)
     submit = SubmitField(ar.register)
 
@@ -26,11 +28,18 @@ class RegisterForm(FlaskForm):
         if qr.doesValueExist(table='users', column='email', value=email.data):
             raise ValidationError('هذا سبق استعماله, اختر غيره أو سجّل دخولك به')
 
+class ContextForm(Form):
+    translationeseContext = StringField('السياق العرنجي', render_kw={"placeholder": "مثال: كن إيجابي!"}, validators=[Length(max=64)])
+    arabicContext = StringField('السياق الصحيح', render_kw={"placeholder": "مثال: تفاءل!"}, validators=[Length(max=64)])
+
+
+
 
 class AddEntry(FlaskForm):
-    original = StringField('اللفظة الأعجمية مكتوبة بلغتها', render_kw={"placeholder": "مثال: Negatives"}, validators=[DataRequired(), Length(max=32)])
-    translationese = StringField('اللفظة العرنجية مكتوبة بالعربية', render_kw={"placeholder": "مثال: سلبيات"}, validators=[DataRequired(), Length(max=32)])
+    original = StringField('اللفظة الأعجمية مكتوبة بلغتها:', render_kw={"placeholder": "مثال: Negatives"}, validators=[DataRequired(), Length(max=32)])
+    translationese = StringField('اللفظة العرنجية مكتوبة بالعربية:', render_kw={"placeholder": "مثال: سلبيات"}, validators=[DataRequired(), Length(max=32)])
     origin = SelectField('دخيل من', validators=[DataRequired()], choices=list(ar.languages.items()))
-    corrections = TextAreaField('المعنى المراد (اختياري), افصل بين المعاني بفواصل', render_kw={"placeholder": "مثال: مساوئ, عيوب"}, validators=[Length(max=256)])
+    corrections = TextAreaField('المعنى المراد (اختياري), افصل بين المعاني بفواصل:', render_kw={"placeholder": "مثال: مساوئ, عيوب"}, validators=[Length(max=256)])
+    context = FormField(ContextForm, "سياق ورود المعنى (اختياري):")
 
     submit = SubmitField("أضف")
